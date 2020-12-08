8b378011c2fb4bf48877eab3c55341696affc800
Razvan Sbingu <razsbg@192-168-0-103.rdsnet.ro>
2020-12-08 14:53:33 +0200

    routing with params

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 function Page() {
-  return <div>Note page</div>;
+  var router = useRouter();
+  const { id } = router.query;
+
+  return <div>Note id: {id}</div>;
 }
 
 export default Page;
